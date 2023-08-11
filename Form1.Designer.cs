86590e98b7c7f43a0fa92ff24e5e3c84eec1a4ea
DeHess <nathhess@gmail.com>
2023-08-11 20:24:16 +0200

    Added first text field for pageNo input

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,47 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.pageNoTextBox = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Location = new System.Drawing.Point(840, 483);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(156, 64);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Grab!";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // pageNoTextBox
             // 
-            this.button2.Location = new System.Drawing.Point(429, 79);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(289, 138);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.pageNoTextBox.Location = new System.Drawing.Point(111, 29);
+            this.pageNoTextBox.Name = "pageNoTextBox";
+            this.pageNoTextBox.Size = new System.Drawing.Size(147, 20);
+            this.pageNoTextBox.TabIndex = 1;
+            this.pageNoTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(111, 74);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(147, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "pageNo";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(1029, 571);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pageNoTextBox);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox pageNoTextBox;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -9,12 +9,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using WindowsFormsApp1.ch.simap.www;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
         SoapServerService client = new SoapServerService();
+        int pageNo = 1;
+        int recordsPerPage = 10;
+        String timespanValue = "YEAR";
+
+
         public Form1()
         {
             InitializeComponent();
@@ -27,27 +33,28 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-
-            
             //SearchNoticeList
-            String searchXml = " <search pageNo=\"1\" recordsPerPage=\"20\">\r\n      <field name=\"NOTICE_NR\"><value>1347901</value></field>\r\n        </search>";
+            //String searchXml = " <search pageNo=\"1\" recordsPerPage=\"20\">\r\n      <field name=\"NOTICE_NR\"><value>1347901</value></field>\r\n        </search>";
 
-            //SearchNoticeCount
-            /*
-            int pageNo = 1;
-            int recordsPerPage = 10;
-            String timespanValue = "YEAR";
             String searchXml = "<search pageNo=\"" + pageNo
-            + "\" recordsPerPage =\"" + recordsPerPage + "\" >"
-            + "<field name =\"TIMESPAN\"><value>" + timespanValue
-            + "</value></field></search>";
-            */
+                + "\" recordsPerPage =\"" + recordsPerPage + "\" >"
+                + "<field name =\"TIMESPAN\"><value>" + timespanValue
+                + "</value></field></search>";
 
             try
             {
                 //SearchNoticeList
                 long?[] h = client.getSearchNoticeList(searchXml);
-                MessageBox.Show(h[0].ToString());
+                String result = "";
+                foreach (long? notice in h) {
+                    result += notice;
+                    result += "|";
+                }
+                
+                MessageBox.Show(result);
+
+                //var h = client.getNoticeHtml(1347901);
+                //MessageBox.Show(h);
 
                 //SearchNoticeCount
                 //var h = client.getSearchNoticeCount(searchXml);
@@ -57,6 +64,19 @@ namespace WindowsFormsApp1
             {
                 MessageBox.Show(ex.ToString());
             }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (int.TryParse(pageNoTextBox.Text, out int newPageNo))
+            {
+                pageNo = newPageNo;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
